0b30675da94ca6dd399ed329b2bc11a0fd6988ea
kingdapster <sal.carrazco89@gmail.com>
2023-11-13 14:00:49 -0800

    im not feelign well

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 
 import './App.css'
+import { puppyList } from './data'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
 
 
   return (
-    <div>
-   
+    <div className='App'>
+   {
+    puppies.map((puppy) => {
+      return<p key={puppy.id}>{puppy.name}</p>
+    })
+   }
     </div>
   )
 }
